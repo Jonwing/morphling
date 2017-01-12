5c8a8d5dd8932e040b0600eb1ee8ab239641f198
lAzUr <jonwing.lee@gmail.com>
2017-01-12 21:16:19 +0800

    changed the renderer interfaces

--- morphling/renderer.py
+++ morphling/renderer.py
@@ -8,7 +8,10 @@ class Renderer(object):
     '''
     _escape_pattern = re.compile(r'&(?!#?\w+;)')
     _not_allowed_schemes = ['javascript:', 'vbscript:']
+
+    # HTML tags
     _p = 'p'
+    _tr = 'tr'
 
     def __init__(self, **kwargs):
         self._escape = kwargs.get('escape', True)
@@ -41,11 +44,9 @@ class Renderer(object):
             content = self._escape_pattern.sub('&amp;', content)
         else:
             content = content.replace('&', '&amp;')
-        content = content.replace('<', '&lt;')
-        content = content.replace('>', '&gt;')
+        content = content.replace('<', '&lt;').replace('>', '&gt;')
         if quote:
-            content = content.replace('"', '&quot;')
-            content = content.replace("'", '&#39;')
+            content = content.replace('"', '&quot;').replace("'", '&#39;')
         return content
 
     def escape_link(self, link):
@@ -57,45 +58,53 @@ class Renderer(object):
 
     def open_tag(self, tag, **kwargs):
         extras = ['%s=%s' % (k, v) for k, v in kwargs.items() if v]
+        tag = getattr(self, ''.join(['_', tag]), tag)
         return '<{tag} {attrs}>'.format(tag=tag, attrs=' '.join(extras))
 
     def close_tag(self, tag, breakline=False):
+        tag = getattr(self, ''.join(['_', tag]), tag)
         if breakline:
             return '</%s>\n' % tag
         return '</%s>' % tag
 
-    def block_html(self, tag, content, **kwargs):
+    def block_html(self, tag, content, breakline=True, **kwargs):
         fmt = '{open_t}{cnt}{close_t}'
         return fmt.format(
-            open_t=self.open_tag(tag, **kwargs), cnt=content, close_t=self.close_tag(tag))
+            open_t=self.open_tag(tag, **kwargs),
+            cnt=content,
+            close_t=self.close_tag(tag, breakline=breakline)
+        )
 
-    def tr(self, content):
-        return '<tr>\n%s</tr>\n' % content
+    def tr(self, content, **kwargs):
+        return self.block_html('tr', content, **kwargs)
+
+    def table(self, header, body):
+        return (
+            '<table>\n<thead>%s</thead>\n'
+            '<tbody>\n%s</tbody>\n</table>\n'
+        ) % (header, body)
 
     def code(self, content):
-        return '<code>%s</code>' % content
+        return self.block_html('code', content, False)
 
     def emphasis(self, content):
-        return '<em>%s</em>' % content
+        return self.block_html('em', content, False)
 
     def double_emphasis(self, content):
-        return '<strong>%s</strong>' % content
-
-    def open_del(self):
-        return '<del>'
-
-    def close_del(self):
-        return '</del>'
+        return self.block_html('strong', content, False)
 
     def strikethrough(self, content):
-        return '{open_tag}{content}{close_tag}'.format(
-            open_tag=self.open_del(), content=content, close_tag=self.close_del())
+        return self.block_html('del', content, False)
 
     def footnote_ref(self, ref_key, index):
-        return '<sup><a class=footnote href=#fn:%s>%s</a></sup>' % (ref_key, index)
+        hyperlink = self.block_html(
+            'a', index, breakline=False, **{'class': 'footnote', 'href': '#fn:%s' % ref_key})
+        return self.block_html('sup', hyperlink, False)
+        # return '<sup><a class=footnote href=#fn:%s>%s</a></sup>' % (ref_key, index)
 
     def link(self, addr, text):
-        return '<a href={addr}>{text}<a>'.format(addr=addr, text=text)
+        return self.block_html('a', text, breakline=False, href=addr)
+        # return '<a href={addr}>{text}<a>'.format(addr=addr, text=text)
 
     def img(self, src, alt=None, title=None):
         seg = '<img src=%s' % self.escape_link(src) if self._escape else src
